376b8aa1cbd71ecd467f287a4925028996c0f9c4
Francary <francarymolinapaez@gmail.com>
2023-11-21 22:45:24 -0300

    Cofiguracion de Variables de entorno

--- src/settings/config.js
+++ src/settings/config.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 
 export const config = {
-  port: process.env.PORT || 3000,
-  mongo: process.env.MONGO_URI || 'mongodb://localhost:27017/express-mongo',
-  jwt_secret: process.env.JWT_SECRET || 'secret',
-  database: process.env.DATABASE_NAME || 'playlist-app',
+  port: process.env.PORT ,
+  mongo: "mongodb://127.0.0.1:27017/" || process.env.MONGO_URI,
+  jwt_secret:  process.env.JWT_SECRET ,
+  database: process.env.DATABASE_NAME ,
 };
