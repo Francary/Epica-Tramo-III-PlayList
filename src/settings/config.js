6387471e71f76d751b0d8f1d1d8e34fe3baab1f1
Francary <francarymolinapaez@gmail.com>
2023-11-26 09:36:08 -0300

    Se modifico direccion de la Base de Datos

--- src/settings/config.js
+++ src/settings/config.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export const config = {
   port: process.env.PORT ,
-  mongo:  process.env.MONGO_URI ,
+  mongo:  "mongodb://127.0.0.1:27017/" || process.env.MONGO_URI ,
   jwt_secret:  process.env.JWT_SECRET ,
   database: process.env.DATABASE_NAME ,
 };
